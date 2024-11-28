ebc944706c6fa089575e69f8f243e105b5ca9a20
Chander Valle <chandez@gmail.com>
2024-11-27 21:29:26 -0500

    initial commit

---
+++ power-shell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+oh-my-posh init pwsh --config $env:NON_THEMES_PATH\non-terminal-minimal.omp.toml | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+Import-Module -Name PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
